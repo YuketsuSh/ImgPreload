06bdc0afb03b7b02c1c7b21fe2490514bbf9ab44
Yuketsu <145769608+YuketsuSh@users.noreply.github.com>
2023-11-16 22:03:53 +0100

    Update ImgPreload.js
    - Adding caching elements 
    - Adding cache max size

--- ImgPreload.js
+++ ImgPreload.js
@@ -1,28 +1,47 @@
+const cache = {};
+const maxCacheSize = 50; // elements limit in the cache
+
 function preloadImage(url) {
     return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => {
-            resolve(img.src);
-        };
-        img.onerror = reject;
-        img.src = url;
+        if (cache[url]) {
+            console.log(`L'image ${url} est déjà dans le cache.`);
+            resolve(cache[url].src);
+        } else {
+            const img = new Image();
+            img.onload = () => {
+                cache[url] = img;
+                console.log(`Image ${url} chargée et mise en cache.`);
+                if (Object.keys(cache).length > maxCacheSize){
+                    const keys = Object.keys(cache);
+                    delete cache[keys[0]];
+                }
+                resolve(img.src);
+            };
+            img.onerror = reject;
+            img.src = url;
+        }
     });
 }
 
 function preloadMedia(url, mediaElement) {
     return new Promise((resolve, reject) => {
-        mediaElement.oncanplaythrough = () => {
-            resolve(url);
-        };
-        mediaElement.onerror = reject;
-        mediaElement.src = url;
+        if (cache[url]) {
+            resolve(cache[url]);
+        } else {
+            mediaElement.oncanplaythrough = () => {
+                cache[url] = mediaElement;
+                resolve(url);
+            };
+            mediaElement.onerror = reject;
+            mediaElement.src = url;
+        }
     });
 }
 
 async function preloadAndShowImage(url, imgElement) {
     try {
-        await preloadImage(url);
-        imgElement.src = url;
+        const cachedImage = await preloadImage(url);
+        imgElement.src = cachedImage;
         console.log(`Préchargement et affichage réussis : ${url}`);
     } catch (error) {
         console.error(`Erreur lors du préchargement : ${url}`, error);
@@ -30,10 +49,14 @@ async function preloadAndShowImage(url, imgElement) {
     }
 }
 
+
 async function preloadAndShowMediaResource(url, mediaElement) {
     try {
-        await preloadMedia(url, mediaElement);
-        mediaElement.src = url;
+        if (!cache[url]) {
+            const cachedMedia = await preloadMedia(url, mediaElement);
+            cache[url] = cachedMedia;
+        }
+        mediaElement.src = cache[url].src;
         console.log(`Préchargement et affichage réussis : ${url}`);
     } catch (error) {
         console.error(`Erreur lors du préchargement : ${url}`, error);
@@ -45,7 +68,7 @@ async function preloadAndApplyBackground() {
     const elementsWithDataBg = document.querySelectorAll('[data-background]');
     for (const element of elementsWithDataBg) {
         const dataBg = element.getAttribute('data-background');
-        if (dataBg) {
+        if (dataBg && !cache[dataBg]) {
             try {
                 await preloadImage(dataBg);
                 element.style.backgroundImage = `url('${dataBg}')`;
@@ -61,7 +84,7 @@ async function preloadAndShowImages() {
     const imgElements = document.querySelectorAll('img[data-src]');
     for (const img of imgElements) {
         const dataSrc = img.getAttribute('data-src');
-        if (dataSrc) {
+        if (dataSrc && !cache[dataSrc]) {
             await preloadAndShowImage(dataSrc, img);
         }
     }
@@ -74,14 +97,14 @@ async function preloadAndShowMedia() {
 
     for (const audio of audioElements) {
         const dataSrc = audio.getAttribute('data-src');
-        if (dataSrc) {
+        if (dataSrc && !cache[dataSrc]) {
             await preloadAndShowMediaResource(dataSrc, audio);
         }
     }
 
     for (const video of videoElements) {
         const dataSrc = video.getAttribute('data-src');
-        if (dataSrc) {
+        if (dataSrc && !cache[dataSrc]) {
             await preloadAndShowMediaResource(dataSrc, video);
         }
     }
